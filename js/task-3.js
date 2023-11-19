973a2caf4367bab902ac99090bb760e6d2c6add3
Vanlsh <92462786+Vanlsh@users.noreply.github.com>
2023-11-19 17:10:35 +0200

    rename variables

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,8 @@
 function getElementWidth(content, padding, border) {
-  const contentWidthNumber = parseFloat(content);
-  const paddingSum = parseFloat(padding) * 2;
+  const contentWidth = parseFloat(content);
+  const paddingWidth = parseFloat(padding) * 2;
   const borderWidth = parseFloat(border) * 2;
-  return contentWidthNumber + paddingSum + borderWidth;
+  return contentWidth + paddingWidth + borderWidth;
 }
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
